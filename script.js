62060c8f0ac3cbb85fd4dedfae45500dd3633937
O Shah <oshah81@outlook.com>
2022-02-08 11:00:34 +0000

    upfate feb 22

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 async function exportToWord(event) {
+    const importProm = import("html-to-docx");
+    const mainElem = document.querySelector("main");
+    const { default: generateContainer } = await importProm;
 
+    const docXRes = await generateContainer(mainElem.innerHTML, null, {
+        orientation: "portrait",
+        title: document.title,
+        lastModifiedBy: document.getElementById("meta[name='author']")?.content ?? "CV",
+        font: "Arial",
+        footer: true,
+        pageNumber: true
+    }, null);
+    window.location = URL.createObjectURL(docXRes);
 }
 
 
